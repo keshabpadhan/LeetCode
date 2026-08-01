e986753646a1263d262a3ac9ac254a7fad582478
Keshab Padhan <thekeshabpadhan@gmail.com>
2026-08-01 07:13:09 +0530

    Time: 0 ms (100%), Space: 42.5 MB (94.79%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,15 +1,14 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-
-        for (int l = n - 1; l >= 0; l--) {
-            dp[l] = nums[l];
-            for (int r = l + 1; r < n; r++) {
-                dp[r] = Math.max(nums[l] - dp[r], nums[r] - dp[r - 1]);
+        if (n % 2 == 0) return true;
+        
+        int[] dp = nums.clone();
+        for (int i = n - 2; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
             }
         }
-
         return dp[n - 1] >= 0;
     }
-}
+} 
