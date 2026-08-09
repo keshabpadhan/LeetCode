547f2a0072a40ecd49bd9ffe08ec818dbc8858a4
Keshab Padhan <thekeshabpadhan@gmail.com>
2026-08-09 19:36:27 +0530

    Time: 2 ms (100%), Space: 44 MB (88.69%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.java
+++ 1140-stone-game-ii/1140-stone-game-ii.java
@@ -1,25 +1,46 @@
 class Solution {
+
+    private int[][] memo;
+    private int[] suffix;
+    private int n;
+
     public int stoneGameII(int[] piles) {
-        int n = piles.length;
-        int[][] dp = new int[n][n + 1];
-        int[] prefixSum = new int[n + 1];
 
-        // Compute prefix sum array
-        for (int i = 0; i < n; i++) {
-            prefixSum[i + 1] = prefixSum[i] + piles[i];
-        }
+        n = piles.length;
 
-        // Fill DP table from the end to the start
+        suffix = new int[n + 1];
+
         for (int i = n - 1; i >= 0; i--) {
-            for (int m = 1; m <= n; m++) {
-                for (int x = 1; x <= 2 * m && i + x <= n; x++) {
-                    int currentStones = prefixSum[i + x] - prefixSum[i];
-                    int opponentStones = (i + x < n) ? dp[i + x][Math.max(m, x)] : 0;
-                    dp[i][m] = Math.max(dp[i][m], currentStones + (prefixSum[n] - prefixSum[i + x] - opponentStones));
-                }
-            }
+            suffix[i] = suffix[i + 1] + piles[i];
         }
 
-        return dp[0][1];
+        memo = new int[n][n + 1];
+
+        return dfs(0, 1);
+    }
+
+    private int dfs(int i, int M) {
+
+        if (i >= n)
+            return 0;
+
+        if (2 * M >= n - i)
+            return suffix[i];
+
+        if (memo[i][M] != 0)
+            return memo[i][M];
+
+        int best = 0;
+
+        for (int X = 1; X <= 2 * M; X++) {
+
+            best = Math.max(
+                    best,
+                    suffix[i] - dfs(i + X, Math.max(M, X))
+            );
+        }
+
+        memo[i][M] = best;
+        return best;
     }
 }
