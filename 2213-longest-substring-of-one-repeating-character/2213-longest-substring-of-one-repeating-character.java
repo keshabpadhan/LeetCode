5645f8948c40e35bf147cc57d5aa1eb2fb4e823b
Keshab Padhan <thekeshabpadhan@gmail.com>
2026-08-13 17:36:22 +0530

    Time: 82 ms (100%), Space: 108.7 MB (75%) - LeetHub

--- 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.java
+++ 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.java
@@ -1,87 +1,141 @@
 class Solution {
-    static class Node {char leftChar; char rightChar; int length; int prefix; int suffix;
-int best;
-        Node(char leftChar, char rightChar, int length, int prefix, int suffix, int best) {
-            this.leftChar = leftChar;
-            this.rightChar = rightChar;
-            this.length = length;
-            this.prefix = prefix;
-            this.suffix = suffix;
-            this.best = best;
+
+    private static class SegmentTree {
+
+        private final int n;
+        private final int[] pre;
+        private final int[] suf;
+        private final int[] best;
+        private final char[] cs;
+
+        public SegmentTree(String s) {
+
+            n = s.length();
+
+            pre = new int[n << 2];
+            suf = new int[n << 2];
+            best = new int[n << 2];
+
+            cs = s.toCharArray();
+
+            build(1, 0, n - 1);
+        }
+
+        private void build(int node, int l, int r) {
+
+            // Leaf node
+            if (l == r) {
+                pre[node] = 1;
+                suf[node] = 1;
+                best[node] = 1;
+                return;
+            }
+
+            int mid = (l + r) >>> 1;
+
+            build(node << 1, l, mid);
+            build(node << 1 | 1, mid + 1, r);
+
+            pushUp(node, l, r);
+        }
+
+        private void pushUp(int node, int l, int r) {
+
+            int left = node << 1;
+            int right = node << 1 | 1;
+
+            int mid = (l + r) >>> 1;
+
+            int lenL = mid - l + 1;
+            int lenR = r - mid;
+
+            // Prefix initially comes from the left child
+            pre[node] = pre[left];
+
+            // Suffix initially comes from the right child
+            suf[node] = suf[right];
+
+            // Best answer is initially the best of either child
+            best[node] = Math.max(best[left], best[right]);
+
+            // Can the repeating sequence cross the boundary?
+            if (cs[mid] == cs[mid + 1]) {
+
+                // Entire left segment has the same character
+                if (pre[left] == lenL) {
+                    pre[node] = lenL + pre[right];
+                }
+
+                // Entire right segment has the same character
+                if (suf[right] == lenR) {
+                    suf[node] = lenR + suf[left];
+                }
+
+                // Combine suffix of left + prefix of right
+                best[node] = Math.max(
+                    best[node],
+                    suf[left] + pre[right]
+                );
+            }
+        }
+
+        public void update(int i) {
+            update(1, 0, n - 1, i);
+        }
+
+        private void update(int node, int l, int r, int i) {
+
+            // Reached the leaf
+            if (l == r) {
+                return;
+            }
+
+            int mid = (l + r) >>> 1;
+
+            if (i <= mid) {
+                update(node << 1, l, mid, i);
+            } else {
+                update(node << 1 | 1, mid + 1, r, i);
+            }
+
+            // Recalculate this node
+            pushUp(node, l, r);
+        }
+
+        public void updateChar(char c, int i) {
+            cs[i] = c;
         }
     }
 
-    private Node[] tree;
+    public int[] longestRepeating(
+        String s,
+        String queryCharacters,
+        int[] queryIndices
+    ) {
 
-    private Node merge(Node left, Node right) {
-        int length = left.length + right.length;
-        int prefix = left.prefix;
+        int k = queryIndices.length;
 
-        if (left.rightChar == right.leftChar && left.prefix == left.length) {
-            prefix = left.length + right.prefix;
+        SegmentTree tree = new SegmentTree(s);
+
+        int[] ans = new int[k];
+
+        for (int i = 0; i < k; i++) {
+
+            int index = queryIndices[i];
+
+            // Change the character
+            tree.updateChar(
+                queryCharacters.charAt(i),
+                index
+            );
+
+            // Update the Segment Tree
+            tree.update(index);
+
+            // Root stores the answer for the entire string
+            ans[i] = tree.best[1];
         }
 
-        int suffix = right.suffix;
-
-        if (left.rightChar == right.leftChar && right.suffix == right.length) {
-            suffix = right.length + left.suffix;
-        }
-
-        int best = Math.max(left.best, right.best);
-
-        if (left.rightChar == right.leftChar) {
-            best = Math.max(best, left.suffix + right.prefix);
-        }
-
-        return new Node(left.leftChar, right.rightChar, length, prefix, suffix, best);
-    }
-
-    private void build( int node, int start, int end, String s) {
-        if (start == end) {
-            tree[node] = new Node( s.charAt(start), s.charAt(start), 1, 1, 1, 1);
-            return;
-        }
-
-        int mid = (start + end) / 2;
-
-        build(node * 2, start, mid, s);
-        build(node * 2 + 1, mid + 1, end, s);
-
-        tree[node] = merge(tree[node * 2],tree[node * 2 + 1]
-        );
-    }
-
-    private void update( int node, int start, int end, int index, char ch) {
-        if (start == end) {
-            tree[node] = new Node(ch, ch, 1, 1, 1, 1);
-            return;
-        }
-
-        int mid = (start + end) / 2;
-
-        if (index <= mid) {
-            update(node * 2, start, mid, index, ch);
-        } else {
-            update(node * 2 + 1, mid + 1, end, index, ch);
-        }
-
-        tree[node] = merge(
-            tree[node * 2],
-            tree[node * 2 + 1]
-        );
-    }
-
-    public int[] longestRepeating(String s, String queryCharacters, int[] queryIndices) {
-        int n = s.length();
-        tree = new Node[4 * n];
-        build(1, 0, n - 1, s);
-        int[] answer = new int[queryIndices.length];
-
-        for (int i = 0; i < queryIndices.length; i++) {
-            update(1, 0, n - 1, queryIndices[i], queryCharacters.charAt(i));
-            answer[i] = tree[1].best;
-        }
-
-        return answer;
+        return ans;
     }
 }
