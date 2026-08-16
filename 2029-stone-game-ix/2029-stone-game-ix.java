0e43340695456d58c7f198faf0c5aa0dd0c7d1a7
Keshab Padhan <thekeshabpadhan@gmail.com>
2026-08-16 09:41:53 +0530

    Time: 4 ms (75.68%), Space: 114.5 MB (97.97%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.java
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -1,30 +1,15 @@
 class Solution {
     public boolean stoneGameIX(int[] stones) {
+        int[] cnt = new int[3];
 
-        int c0 = 0;
-        int c1 = 0;
-        int c2 = 0;
-
-        // Count stones according to their remainder
         for (int stone : stones) {
-
-            if (stone % 3 == 0) {
-                c0++;
-            } 
-            else if (stone % 3 == 1) {
-                c1++;
-            } 
-            else {
-                c2++;
-            }
+            cnt[stone % 3]++;
         }
 
-        // Case 1: Number of remainder-0 stones is even
-        if (c0 % 2 == 0) {
-            return c1 > 0 && c2 > 0;
+        if (cnt[0] % 2 == 0) {
+            return cnt[1] > 0 && cnt[2] > 0;
         }
 
-        // Case 2: Number of remainder-0 stones is odd
-        return Math.abs(c1 - c2) > 2;
+        return Math.abs(cnt[1] - cnt[2]) > 2;
     }
 }
